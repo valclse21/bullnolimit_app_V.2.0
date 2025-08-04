803e8ac7e0f39811c91f0a36e3eac3c6ec27c372
fidiferdy <fidiferdy21@gmail.com>
2025-08-05 03:59:39 +0700

    Ganti API Twelve Data

--- src/api/keys.js
+++ src/api/keys.js
@@ -1,2 +1,2 @@
-export const TWELVE_DATA_API_KEY = "ece64523230d4463a27aba532c327c4b";
+export const TWELVE_DATA_API_KEY = "53f012569ba3487e9473a0e21e831c8a";
 export const FINNHUB_API_KEY = "d22ihohr01qr7ajlku90d22ihohr01qr7ajlku9g";
