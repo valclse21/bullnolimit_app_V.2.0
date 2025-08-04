d2712eef15d04279e225c0e7e9a0d621d346205c
fidiferdy <fidiferdy21@gmail.com>
2025-08-05 01:44:51 +0700

    refactor: Clean up server-side logs

--- api/scanMarket.js
+++ api/scanMarket.js
@@ -27,9 +27,6 @@ const imageToBase64 = (filePath) => {
 };
 
 export default async function handler(req, res) {
-  console.log('--- SERVER LOG ---');
-  console.log('Attempting to read OPENROUTER_API_KEY...');
-  console.log('API Key available:', !!process.env.OPENROUTER_API_KEY);
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
